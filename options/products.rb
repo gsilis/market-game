6109ec8b21704e36cc1a2dbc6cd9d6e0540ef127
Girts Silis <girts.silis@gmail.com>
2017-04-08 20:48:24 -0500

    Simplify product creation for now
    Remove the code that tried to verify the yml file and just have a
    default set within the products collection.
    
    Will want to have some sort of configurable file later on, but getting
    to a point where the even is a game is the first goal.

--- options/products.rb
+++ options/products.rb
@@ -1,57 +1,38 @@
-require 'yaml'
-
 module Game
   class Products
-    def self.load
-      @config ||= YAML.load_file './config/products.yml'
-      result = verify_config
-      @products = convert_to_products if result
-
-      result
+    def initialize(products = nil)
+      @products = products || default_products
     end
 
-    def self.verify_config
-      products = @config['products']
-      return false if products.nil?
-
-      is_valid = true
-
-      products.each do |product|
-        is_valid = false unless verify_product(product)
-      end
-
-      is_valid
-    end
-
-    def self.verify_product(product)
-      name = product['name']
-      supply_range = Range.new *product['supply_range'].split('..')
-      standard_range = Range.new *product['standard_range'].split('..')
-      demand_range = Range.new *product['demand_range'].split('..')
-
-      return false if name.nil? || supply_range.nil? || standard_range.nil? || demand_range.nil?
-      return false if supply_range.max > standard_range.min || standard_range.max > demand_range.min
-
-      true
-    end
-
-    def convert_to_products
-      @config['products'].map{ |product| Product.new product }
-    end
-
-    def self.all
+    def all
       @products
+    end
+
+    private
+    def default_products
+      [
+        Product.new('Lorem', 1..10),
+        Product.new('Ipsum', 11..30),
+        Product.new('Sumet', 40..75),
+        Product.new('Donec', 100..150)
+      ]
     end
   end
 
+  private
   class Product
-    attr_accessor :name, :supply_range, :standard_range, :demand_range
+    def initialize(name, range)
+      @name = name
+      @range = range
+    end
 
-    def initialize(product)
-      @name = product[:name]
-      @standard_range = Range.new *product['standard_range'].split('..')
-      @supply_range = Range.new *product['supply_range'].split('..')
-      @demand_range = Range.new *product['demand_range'].split('..')
+    def re_range(new_min = nil, new_max = nil)
+      new_min = new_min || @range.min
+      new_max = new_max || @range.max
+
+      return false if new_min >= new_max
+
+      @range = Range.new new_min, new_max
     end
   end
 end
